36e1cabd08248f9e7656645acf9d55113c5f2057
Jung-Young-In <81631456+Jung-Young-In@users.noreply.github.com>
2022-09-29 02:22:34 +0900

    09-28 추가 개발 사항 없음... commit

--- app/src/main/java/com/example/movieaword/MainActivity.kt
+++ app/src/main/java/com/example/movieaword/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.movieaword
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,22 +10,115 @@ import android.view.Window
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.movieaword.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.AuthErrorCause
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
-import com.navercorp.nid.NaverIdLoginSDK
-import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.kakao.usermgmt.StringSet.email
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
 
+
+
+//    private lateinit var binding : ActivityMainBinding
+//    private var TAG = this.javaClass.simpleName
+//
+//    private lateinit var launcher : ActivityResultLauncher<Intent>
+//    private lateinit var firebaseAuth : FirebaseAuth
+//
+//    private var email : String = ""
+//    private var tokenId : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 현재 Naver 로그인 클릭 시 에러 발생 -> 처리 필요
-        // 로그인 화면 Naver 버튼과 카카오 버튼 겹침 -> 처리 필요
+
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_main)
+
+//        firebaseAuth = FirebaseAuth.getInstance()
+//        launcher = registerForActivityResult(
+//            ActivityResultContracts.StartActivityForResult(), ActivityResultCallback { result ->
+//                Log.e(TAG, "resultCode : ${result.resultCode}")
+//                Log.e(TAG, "result : $result")
+//
+//                if(result.resultCode == RESULT_OK) {
+//                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+//                    try {
+//                        task.getResult(ApiException::class.java)?.let { account ->
+//                            tokenId = account.idToken
+//
+//                            if(tokenId != null && tokenId != "") {
+//                                val credential : AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+//                                firebaseAuth.signInWithCredential(credential)
+//                                    .addOnCompleteListener {
+//                                        if(firebaseAuth.currentUser != null) {
+//                                            val user : FirebaseUser = firebaseAuth.currentUser!!
+//                                            email = user.email.toString()
+//                                            Log.e(TAG, "email : $email")
+//                                            val googleSignInToken = account.idToken ?: ""
+//
+//                                            if(googleSignInToken != "") {
+//                                                Log.e(TAG, "googleSignInToken : $googleSignInToken")
+//                                            }else{
+//                                                Log.e(TAG, "googleSignInToken이 Null")
+//                                            }
+//                                        }
+//                                    }
+//                            }
+//                        } ?: throw Exception()
+//                    } catch (e : Exception) {
+//                        e.printStackTrace()
+//                    }
+//                }
+//            })
+////
+//        binding.run {
+//            google_login_button.setOnClickListener {
+//                CoroutineScope(Dispatchers.IO).launch {
+//                    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                        .requestIdToken(getString(R.string.default_web_client_id))
+//                        .requestEmail()
+//                        .build()
+//                    val googleSignInClient = GoogleSignIn.getClient(this@MainActivity, gso)
+//                    val signInIntent : Intent = googleSignInClient.signInIntent
+//                    launcher.launch(signInIntent)
+//                }
+//            }
+//        }
+
+
+
+
+
+
+
+
+//        setContentView(R.layout.activity_main)
 
         // 로그인 정보 확인
         UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
